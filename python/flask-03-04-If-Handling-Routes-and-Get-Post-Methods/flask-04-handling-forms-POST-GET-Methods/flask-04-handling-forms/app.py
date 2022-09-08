02e27c651bca8baa4aac86e10c8f97a380d2bc2a
mehmet <eminunsal20@gmail.com>
2022-09-08 22:53:57 +0200

    .

--- python/flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
+++ python/flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Create welcome page with main.html file and assing it to the root path
 @app.route('/')
 def home():
-    return render_template('main.html', name='Armando')
+    return render_template('main.html', name='Mehmet Emin')
 
 # Write a function named `greet` which uses template file named `greet.html` given under 
 # `templates` folder. it takes parameters from query string on URL, assign that parameter 
