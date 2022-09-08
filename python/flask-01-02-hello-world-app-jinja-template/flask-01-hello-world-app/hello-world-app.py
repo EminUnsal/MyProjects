e0fed8b4c29ac0fc5fa0e34a2ae1f64641598f4d
mehmet <eminunsal20@gmail.com>
2022-09-08 22:38:18 +0200

    If-Handling-Routes-and-Get-Post-Methods

--- python/flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
+++ python/flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
@@ -4,13 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def head():
-    return "<h1>Hello World Mehmet.This is from Clarusway DE</h1>"
+    return "<h1>Hello World Armando This is from Clarusway DE</h1>"
 
 
 @app.route('/second')
 def second():
     return "This is the second page"
-
 
 @app.route('/third/subthird')
 def third():
@@ -19,12 +18,7 @@ def third():
 
 @app.route('/forth/<string:id>')
 def forth(id):
-    return f'Id of thids page is {id}'
-
-
-
-
-
+    return f'Id of this page is {id}'
 
 
 
--- python/flask-01-02-hello-world-app-jinja-template/flask-02-Jinja_Template/Jinja.py
+++ python/flask-01-02-hello-world-app-jinja-template/flask-02-Jinja_Template/Jinja.py
@@ -1,25 +1,20 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def head():
     return render_template('index.html', number1=112500, number2=225200)
 
-
-@app.route('/sum')
+@app.route('/mult')
 def number():
+
     x=15
     y=20
-    return render_template('body.html', num1=x, num2=y, sum=x+y)
+    return render_template('body.html', num1=x, num2=y, mult=x*y)
+
 
 
 if __name__ == '__main__':
-    app.run(port=2000, debug=False)
-
-
-
-
-
-
+    app.run(port=3000, debug=True)
 
