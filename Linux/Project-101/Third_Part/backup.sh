68ce6d351f9747e3453324800072528022ad918a
mehmet <eminunsal20@gmail.com>
2022-09-30 10:03:04 +0200

    Third-Part of Project added

---
+++ Linux/Project-101/Third_Part/backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if we are root privilage or not
+
+
+# Which files are we going to back up. Please make sure to exist /home/ec2-user/data file
+ 
+
+# Where do we backup to. Please crete this file before execute this script
+
+
+# Create archive filename based on time
+
+
+# Print start status message.
+
+
+# Backup the files using tar.
+ # &> /dev/null (Bunu script birkaç kez calistirilip eklenebilir)
+
+# Print end status message.
+
+
+# Long listing of files in $dest to check file sizes.
+
+
+-------------
+# /etc/crontab ın içine yazılacak
+
+
+# Good article -> https://www.cumulations.com/blog/how-to-write-cron-jobs-on-amazon-web-servicesaws-ec2-server/
+
+# To set this script for executing in every 5 minutes, we'll create cronjob
+
+# vi or nano editor will open. We will run  backup.sh script in every 5 minutes. To be able to do this we'll write this within opend vi or nano file
+
+
+
+# save and exit from nano or vi
+
+# To check whether your Cron Jobs is saved or not, run the below command.
+
