491fcd37e64a46731287230782b6293830df003b
mehmet <eminunsal20@gmail.com>
2022-10-06 12:49:46 +0200

    Solution of Third-Part added

--- Linux/Project-101/Third_Part/backup.sh
+++ Linux/Project-101/Third_Part/backup.sh
@@ -6,31 +6,44 @@ if [[ $UID != 0 ]]; then
     echo "You don't have superuser privileges. Make sure to run script as root user."
     exit 1
 fi
-
 # Which files are we going to back up. Please make sure to exist /home/ec2-user/data file
  
+backup_files="/home/ec2-user/data"
 
 # Where do we backup to. Please crete this file before execute this script
 
+destination="/mnt/backup"
 
 # Create archive filename based on time
 
+time=$(date +%Y_%m_%d_%I_%M_%p)
+hostname=$(hostname -s)
+archive_files="${hostname}-${time}.tgz"
 
 # Print start status message.
 
+echo "We will back up ${backup_files} to ${destination}/${archive_files}"
+date
+echo
 
 # Backup the files using tar.
  # &> /dev/null (Bunu script birkaç kez calistirilip eklenebilir)
-
+tar czf ${destination}/${archive_files} ${backup_files}
 # Print end status message.
-
+echo
+echo "Congratulation, your backup is ready"
+date
+echo
 
 # Long listing of files in $dest to check file sizes.
+ls -lh ${destination}
 
-
--------------
+#-------------
 # /etc/crontab ın içine yazılacak
 
+#crontab -e
+
+#*/5 * * * * sudo /home/ec2-user/Third_Part/backup.sh
 
 # Good article -> https://www.cumulations.com/blog/how-to-write-cron-jobs-on-amazon-web-servicesaws-ec2-server/
 
@@ -43,4 +56,5 @@ fi
 # save and exit from nano or vi
 
 # To check whether your Cron Jobs is saved or not, run the below command.
+
 
