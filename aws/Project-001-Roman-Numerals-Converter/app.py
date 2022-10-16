cd355b48b4850fa02258ba61d8ba3854c1e9f25e
mehmet <eminunsal20@gmail.com>
2022-10-16 21:56:25 +0200

    Files modified

--- aws/Project-001-Roman-Numerals-Converter/app.py
+++ aws/Project-001-Roman-Numerals-Converter/app.py
@@ -17,14 +17,14 @@ def main_post():
     if request.method == 'POST':
         alpha = request.form['number']
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Mehmet', not_valid=True)
+            return render_template('index.html', developer_name='Serdar', not_valid=True)
         number = int(alpha)
         if not 0 < number < 4000:
-            return render_template('index.html', developer_name='Mehmet', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Mehmet')
+            return render_template('index.html', developer_name='Serdar', not_valid=True)
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Serdar')
     else:
-        return render_template('index.html', developer_name='Mehmet', not_valid=False)
+        return render_template('index.html', developer_name='Serdar', not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
