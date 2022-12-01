694c8b8f7fc21891ba90cb6bf048fd526007c10d
mehmet <eminunsal20@gmail.com>
2022-12-01 13:07:18 +0100

    main.tf and variables.tf created

---
+++ DevOps/Project-203-Dockerization-Bookstore-Api-on-Python-Flask-Mysql/Project-Solution/variables.tf
@@ -0,0 +1,9 @@
+variable "files" {
+  default = ["bookstore-api.py", "Dockerfile", "docker-compose.yml", "requirements.txt"]
+}
+variable "github-token"{
+    default = "Write-Your-Github-Token"
+}
+variable "keyname"{
+    default = "Write-Your-Key-Name"
+}
