ce9e07c2e3bee27a6c0c3476aab2857e7308c62a
mehmet <eminunsal20@gmail.com>
2022-12-01 13:06:26 +0100

    Docker-compose.yml created

--- DevOps/Project-203-Dockerization-Bookstore-Api-on-Python-Flask-Mysql/Project-Solution/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "files" {
-  default = ["bookstore-api.py", "Dockerfile", "docker-compose.yml", "requirements.txt"]
-}
-variable "github-token"{
-    default = "Write-Your-Github-Token"
-}
-variable "keyname"{
-    default = "Write-Your-Key-Name"
-}
