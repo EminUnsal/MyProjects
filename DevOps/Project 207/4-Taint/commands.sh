f6fb8b7eae72dc4dbf43bbfcdb51a39f62097d16
mehmet <eminunsal20@gmail.com>
2022-12-28 19:47:52 +0100

    toraration argument added

--- DevOps/Project 207/4-Taint/commands.sh
+++ DevOps/Project 207/4-Taint/commands.sh
@@ -1,6 +1,6 @@
 node1=$(kubectl get no -o jsonpath="{.items[1].metadata.name}")
-node2=$(kubectl get no -o jsonpath="{.items[1].metadata.name}")
-node3=$(kubectl get no -o jsonpath="{.items[1].metadata.name}")
+node2=$(kubectl get no -o jsonpath="{.items[2].metadata.name}")
+node3=$(kubectl get no -o jsonpath="{.items[3].metadata.name}")
 kubectl taint no $node1 env=prod:NoSchedule
 kubectl taint no $node2 env=prod:NoSchedule
 kubectl taint no $node3 env=prod:NoSchedule
