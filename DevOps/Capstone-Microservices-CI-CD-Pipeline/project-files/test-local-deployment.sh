275c95d6d718acbeb4cc6119390f3d8e51408f8e
EminUnsal <eminunsal20@gmail.com>
2023-02-27 21:31:18 +0100

    Project files added

---
+++ DevOps/Capstone-Microservices-CI-CD-Pipeline/project-files/test-local-deployment.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose-local.yml up
+# docker-compose -f docker-compose-local.yml up -d --> deamon olarak calisir ve biz ciktilari gormeyiz. durdurmak icin ./test-local-deployment.sh stop
